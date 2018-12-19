f42283bf636928d63f9db6612480e75a661d11d9
chibirevartem <45075481+chibirevartem@users.noreply.github.com>
2018-12-19 23:13:33 +0400

    Add files via upload

--- Epam.Task04/Epam.Task04.2.WordFrequency/Program.cs
+++ Epam.Task04/Epam.Task04.2.WordFrequency/Program.cs
@@ -8,10 +8,9 @@ namespace Epam.Task04._2.WordFrequency
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            string input = "This is my world. My new World";
+            string input = "My family.Meet my family.There are five of us – my parents, my elder brother,my baby sister and me. First, meet my mum and dad.";
             WordFrequency wf = new WordFrequency(input);
             wf.Results_ToConsole();
         }
--- Epam.Task04/Epam.Task04.2.WordFrequency/classes/WordFrequency.cs
+++ Epam.Task04/Epam.Task04.2.WordFrequency/classes/WordFrequency.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data = System.Collections.Generic.KeyValuePair<string, int>;
 
 namespace Epam.Task04._2.WordFrequency
 {
@@ -11,18 +12,14 @@ namespace Epam.Task04._2.WordFrequency
         string input;
         string[] output;
         List<string> list;
-        Dictionary<string, int> check_result;
-
-
+        List<Data> check_result;
         char[] ignored_symbols = { ' ', '.' };
-
         List<string> dismatches = new List<string>();
         public WordFrequency(string text)
         {
             if (text != null)
             {
                 input = text;
-
                 list = new List<string>();
                 output = input.Split(ignored_symbols, StringSplitOptions.RemoveEmptyEntries);
 
@@ -30,11 +27,9 @@ namespace Epam.Task04._2.WordFrequency
                 {
                     char[] charray = output[i].ToArray();
                     string this_word = "";
-
                     for (int j = 0; j < charray.Length; j++)
                     {
                         char this_symbol = charray[j];
-
                         if (char.IsLetter(this_symbol))
                         {
                             this_word += char.ToLower(this_symbol);
@@ -45,42 +40,50 @@ namespace Epam.Task04._2.WordFrequency
 
                 Repeatability_check();
             }
-
-
-
         }
 
         public void Results_ToConsole()
         {
+            //Console.WriteLine($"The text is : {input}\n");
+            //foreach (KeyValuePair<string, int> value in check_result)
+            //{
+            //    Console.WriteLine($"{value.Key.ToString()}: repeats {value.Value.ToString()} times.");
+            //}
+
             Console.WriteLine($"The text is : {input}\n");
+
+            //for (int i = 0; i < check_result.Count; i++)
+            //{
+            //    Console.WriteLine($"{check_result}");
+            //}
+
             foreach (KeyValuePair<string, int> value in check_result)
             {
                 Console.WriteLine($"{value.Key.ToString()}: repeats {value.Value.ToString()} times.");
             }
+
+           
+
+
         }
 
         public void Repeatability_check()
         {
-            check_result = new Dictionary<string, int>();
-
+            check_result = new List<Data>();
             for (int i = 0; i < list.Count; i++)
             {
                 int matches = 0;
-
-
                 for (int j = i + 1; j < list.Count; j++)
                 {
                     if (list[i] == list[j])
                     {
                         matches++;
                         list.RemoveAt(j);
+                        j--;
                     }
                 }
-                check_result.Add(list[i], matches);
-
-
+                check_result.Add(new Data(list[i], matches));
             }
         }
-
     }
 }
